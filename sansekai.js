13c6a669c71d32f96803e7d6391ce20684692e1a
cctv-security <a3cx992@gmail.com>
2024-11-22 08:44:32 +0200

    Initial commit

---
+++ sansekai.js
@@ -0,0 +1,127 @@
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require("@whiskeysockets/baileys");
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const { G4F } = require("g4f");
+const g4f = new G4F();
+let setting = require("./key.json");
+
+module.exports = COder = async (client, m, chatUpdate) => {
+  try {
+    var body = m.mtype === "conversation" ? m.message.conversation :
+           m.mtype == "imageMessage" ? m.message.imageMessage.caption :
+           m.mtype == "videoMessage" ? m.message.videoMessage.caption :
+           m.mtype == "extendedTextMessage" ? m.message.extendedTextMessage.text :
+           m.mtype == "buttonsResponseMessage" ? m.message.buttonsResponseMessage.selectedButtonId :
+           m.mtype == "listResponseMessage" ? m.message.listResponseMessage.singleSelectReply.selectedRowId :
+           m.mtype == "templateButtonReplyMessage" ? m.message.templateButtonReplyMessage.selectedId :
+           m.mtype === "messageContextInfo" ? m.message.buttonsResponseMessage?.selectedButtonId || 
+           m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text :
+           "";
+    if (m.mtype === "viewOnceMessageV2") return
+    var budy = typeof m.text == "string" ? m.text : "";
+    var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/";
+    const isCmd2 = body.startsWith(prefix);
+    const command = body.replace(prefix, "").trim().split(/ +/).shift().toLowerCase();
+    const args = body.trim().split(/ +/).slice(1);
+    const pushname = m.pushName || "No Name";
+    const botNumber = await client.decodeJid(client.user.id);
+    const itsMe = m.sender == botNumber ? true : false;
+    let text = (q = args.join(" "));
+    const arg = budy.trim().substring(budy.indexOf(" ") + 1);
+    const arg1 = arg.trim().substring(arg.indexOf(" ") + 1);
+
+    const from = m.chat;
+    const reply = m.reply;
+    const sender = m.sender;
+    const mek = chatUpdate.messages[0];
+
+    const color = (text, color) => {
+      return !color ? chalk.green(text) : chalk.keyword(color)(text);
+    };
+
+    // Group
+    const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch((e) => {}) : "";
+    const groupName = m.isGroup ? groupMetadata.subject : "";
+
+    // Push Message To Console
+    let argsLog = budy.length > 30 ? `${q.substring(0, 30)}...` : budy;
+
+    if (isCmd2 && !m.isGroup) {
+      console.log(chalk.black(chalk.bgWhite("[ LOGS ]")), color(argsLog, "turquoise"), chalk.magenta("From"), chalk.green(pushname), chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`));
+    } else if (isCmd2 && m.isGroup) {
+      console.log(
+        chalk.black(chalk.bgWhite("[ LOGS ]")),
+        color(argsLog, "turquoise"),
+        chalk.magenta("From"),
+        chalk.green(pushname),
+        chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`),
+        chalk.blueBright("IN"),
+        chalk.green(groupName)
+      );
+    }
+
+    if (isCmd2) {
+      switch (command) {
+        
+        case "ai": case "gpt": case "*": case "ask":
+          try {
+            if (!text) return reply(`✌️\n\n Tira Ai coder \n\n ~|t|i|r|a|☆|A|~`);
+
+            const messages = [
+                { role: "system", content: "you are name is Tira Ai."},
+                { role: "user", content: q },
+            ];
+            const options = {
+                model: "gpt-4",
+                debug: false,
+                retry: {
+                    times: 1,
+                    condition: (text) => {
+                        const words = text.split("•••••• ");
+                        return words.length > 10;
+                    }
+                },
+                output: (text) => {
+                    return text + " \n\n ~Tira Coder~";
+                }
+            };
+
+            const chatCompletion = await g4f.chatCompletion(messages, options);
+          
+            await m.reply(chatCompletion);
+          } catch (error) {
+            console.log(error);
+            m.reply("Maaf, sepertinya ada yang error :" + error.message);
+          }
+          break;
+        case "sc": case "script": case "scbot":
+          m.reply("Tira Bot start..✅️");
+          break;
+        default: {
+          if (isCmd2 && budy.toLowerCase() != undefined) {
+            if (m.chat.endsWith("broadcast")) return;
+            if (m.isBaileys) return;
+            if (!budy.toLowerCase()) return;
+            if (argsLog || (isCmd2 && !m.isGroup)) {
+              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("tidak tersedia", "turquoise"));
+            } else if (argsLog || (isCmd2 && m.isGroup)) {
+              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("tidak tersedia", "turquoise"));
+            }
+          }
+        }
+      }
+    }
+  } catch (err) {
+    m.reply(util.format(err));
+  }
+};
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update ${__filename}`));
+  delete require.cache[file];
+  require(file);
+});
+
